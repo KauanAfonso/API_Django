4e7686b9a95edc5f12bce813ee828c3559df9739
KauanAfonso <kauanafon3@gmail.com>
2025-02-16 10:31:34 -0300

    API crud

--- project/API/urls.py
+++ project/API/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_pedido, create_pedido
+from .views import get_pedidos, create_pedido, pedido_detail
 
 urlpatterns = [
-    path('pedidos/', get_pedido, name='get_pedido'),
-    path('pedidos/create', create_pedido, name='create_pedido')
+    path('pedidos/', get_pedidos, name='get_pedidos'),
+    path('pedidos/create', create_pedido, name='create_pedido'),
+    path('pedidos/<int:pk>', pedido_detail, name='pedido_detail'),
 ]
 
--- project/API/views.py
+++ project/API/views.py
@@ -5,9 +5,36 @@ from .serializer import PedidoSerializer
 from .models import Pedido
 # Create your views here.
 
-@api_view(["GET"]) 
-def get_pedido(request):
-    return Response(PedidoSerializer({"nome_pedido": "oi", "descricao": "2341"}).data)
+
+@api_view(["GET"])
+def get_pedidos(request):
+    pedidos = Pedido.objects.all()
+    serializer = PedidoSerializer(pedidos, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET" , "DELETE", "PUT"]) 
+def pedido_detail(request, pk):
+    try:
+        id_pedido = Pedido.objects.get(pk=pk)
+    except Pedido.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        serializer = PedidoSerializer(id_pedido)
+        return Response(serializer.data)
+    
+    if request.method == "DELETE":
+        operacao = id_pedido.delete()
+        if operacao:
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == "PUT":
+        serializer = PedidoSerializer(id_pedido, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["POST"]) 
@@ -17,3 +44,6 @@ def create_pedido(request):
        serializer.save()
        return Response(serializer.data, status=status.HTTP_201_CREATED)
    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
