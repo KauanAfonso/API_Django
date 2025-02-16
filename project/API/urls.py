4aa113ff21322289f285f98441d77b1f0d7f84a4
KauanAfonso <kauanafon3@gmail.com>
2025-02-16 09:50:25 -0300

    Adicionando o post

--- project/API/urls.py
+++ project/API/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_pedido
+from .views import get_pedido, create_pedido
 
 urlpatterns = [
-    path('pedidos/', get_pedido, name='get_pedido')
+    path('pedidos/', get_pedido, name='get_pedido'),
+    path('pedidos/create', create_pedido, name='create_pedido')
 ]
 
--- project/API/views.py
+++ project/API/views.py
@@ -8,3 +8,12 @@ from .models import Pedido
 @api_view(["GET"]) 
 def get_pedido(request):
     return Response(PedidoSerializer({"nome_pedido": "oi", "descricao": "2341"}).data)
+
+
+@api_view(["POST"]) 
+def create_pedido(request):
+   serializer = PedidoSerializer(data=request.data)
+   if serializer.is_valid():
+       serializer.save()
+       return Response(serializer.data, status=status.HTTP_201_CREATED)
+   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
